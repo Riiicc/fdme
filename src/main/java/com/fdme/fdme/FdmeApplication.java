6018310425a95998c507884a7b994557810b0f31
riiicc <Riiicc@hotmail.com>
2019-08-23 16:44:40 +0800

    first commit

---
+++ src/main/java/com/fdme/fdme/FdmeApplication.java
@@ -0,0 +1,13 @@
+package com.fdme.fdme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FdmeApplication.class, args);
+	}
+
+}
