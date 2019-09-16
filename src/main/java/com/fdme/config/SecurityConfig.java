4942dc0610560ed21d403263dcc14eadba5f632b
riiicc <Riiicc@hotmail.com>
2019-09-16 11:08:31 +0800

    work1

--- src/main/java/com/fdme/config/SecurityConfig.java
+++ src/main/java/com/fdme/config/SecurityConfig.java
@@ -23,76 +23,29 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Bean
-    protected UserDetailsService customUserService() {
-        return new CustomUserService();
-    }
+	private UserDetailsService userDetailsService;
 
-    @Autowired
-    public void configureAuthentication(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder
-                // 设置UserDetailsService
-                .userDetailsService(customUserService())
-                // 使用BCrypt进行密码的hash
-                .passwordEncoder(passwordEncoder());
-    }
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    // 装载BCrypt密码编码器
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	public SecurityConfig(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+		this.userDetailsService = userDetailsService;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
 
-//    @Bean
-//    public JwtAuthenticationTokenFilter authenticationTokenFilterBean() throws Exception {
-//        return new JwtAuthenticationTokenFilter();
-//    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable().authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/users/signup").permitAll()
+				.anyRequest().authenticated()
+				.and()
+				.addFilter(new JWTLoginFilter(authenticationManager()))
+				.addFilter(new JWTAuthenticationFilter(authenticationManager()));
+	}
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserService());
-    }
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+	}
 
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                // 由于使用的是JWT，我们这里不需要csrf
-//                .csrf().disable()
-                // 基于token，所以不需要session
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-
-                .authorizeRequests()
-
-                //.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                // 允许对于网站静态资源的无授权访问
-                .antMatchers(
-                        HttpMethod.GET,
-                        "/",
-                        "/*.html",
-                        "/favicon.ico",
-                        "/**/*.html",
-                        "/**/*.css",
-                        "/**/*.js"
-                ).permitAll()
-                // 对于获取token的rest api要允许匿名访问
-//                .antMatchers("/auth/**").permitAll()
-//                .antMatchers("/admin/**").hasIpAddress("127.0.0.1")
-//                .antMatchers("/admin/**").access("hasAuthority('ROLE_ADMIN')")
-                .anyRequest().authenticated().and().formLogin().loginPage("/login")
-                .failureUrl("/login?error").permitAll().and().logout().permitAll();
-        // 除上面外的所有请求全部需要鉴权认证
-//				.anyRequest().authenticated();
-        // 添加JWT filter
-//        httpSecurity
-//                .addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
-        // 禁用缓存
-//        httpSecurity.headers().cacheControl();
-    }
 
 }
